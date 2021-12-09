50a1b374780f3e1ebf999fb2d2f4dac3a70c10f7
Phung Dinh Son <62043279+phungson185@users.noreply.github.com>
2021-12-09 22:13:59 +0700

    fix: bug when login (#9)
    fix: bug when login (#9)

--- src/Components/ChatWindow.js
+++ src/Components/ChatWindow.js
@@ -64,7 +64,7 @@ const ChatWindow = ({socket, room, leave}) => {
     useEffect(() => {
         setRoom(room)
         if(room?._id !== -1)
-            axios.get('/message/room/' + room?.id, {withCredentials: true}).then(response => {
+            axios.get('/message/room/' + room?._id, {withCredentials: true}).then(response => {
                 setDialogs(response.data.msg)
             })
     }, [room])
