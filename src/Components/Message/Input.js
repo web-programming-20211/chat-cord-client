d2863f63b3324a1f3b56dde974435d413c75c48a
UongMinhTan <minhtan471100@gmail.com>
2021-12-27 01:12:27 +0700

    add font and preview selected file

--- src/Components/Message/Input.js
+++ src/Components/Message/Input.js
@@ -19,6 +19,7 @@ const Input = ({ setDialogs }) => {
     const [showEmoji, setShowEmoji] = useState(false)
     const [files, setFiles] = useState([]);
     const [urls, setUrls] = useState([]);
+    const [previewImage, setPreviewImage] = useState([{id: null, url: null}]);
 
     const limit = useMediaQuery({ maxWidth: 900 })
     const limit2 = useMediaQuery({ maxWidth: 600 })
@@ -79,15 +80,56 @@ const Input = ({ setDialogs }) => {
             cursor: 'pointer',
             right: 45,
             bottom: 14
+        },
+
+        preview: {
+            zIndex: 1,
+            position: 'absolute',
+            width: '100%',
+            height: '200px',
+            right: '1px',
+            bottom: '60px',
+            background: '#6588DE',
+            boxShadow: '0px 8px 40px rgba(0, 72, 251, 0.3)',
+            borderRadius: '14px',
+            padding: '20px',
+            display: 'flex',
+            // justifyContent: 'center',
+            alignItems: 'center',
+        },
+
+        thumbnail: {
+            width: '120px',
+            height: '100px'
+        },
+
+        deletePreviewIcon: {
+            color: 'red',
+            fontSize: '20px',
+            cursor: 'pointer',
+            position: 'relative',
+            top: '-56px',
         }
     }
 
     const handleEmojiSelect = (e) => { setMessage((message) => (message += e.native)) }
 
+    const deletePreview = (id) => {
+        setPreviewImage(previewImage.filter(item => item.id !== id))
+        setFiles(files.filter(item => item.id !== id))
+    }
+    
     const handleChange = (e) => {
+        // convert to base64
         for (let i = 0; i < e.target.files.length; i++) {
             const newFile = e.target.files[i]
+            console.log(newFile)
             newFile["id"] = Math.random()
+            const reader = new FileReader()
+            reader.readAsDataURL(newFile)
+            reader.onloadend = () => {
+                setPreviewImage((prevState) => [...prevState, { id: newFile["id"], url: reader.result}])
+            }
             setFiles((prevState) => [...prevState, newFile])
         }
     }
@@ -98,7 +140,7 @@ const Input = ({ setDialogs }) => {
             files.map((file) => {
                 const promise = new Promise((resolve, reject) => {
                     const metadata = {
-                        contentType: file.type
+                        contentType: file.type 
                     }
                     const storageRef = storage.ref(`files/${file.name}`)
                     storageRef.put(file, metadata).then((snapshot) => {
@@ -114,6 +156,23 @@ const Input = ({ setDialogs }) => {
                 setDialogs(message, urls)
             })
         }
+        setPreviewImage([])
+        setFiles([])
+    }
+    const PreviewSelectedFiles = () => {
+        // const reader = new FileReader()
+        return (
+            <div style={style.preview}>
+                {previewImage.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <Icon onClick={() => deletePreview(item.id)} style={style.deletePreviewIcon} icon="ep:circle-close" />
+                            <img style={style.thumbnail} src={item.url} alt="thumb" />
+                        </div>
+                    )
+                })}
+            </div>
+        )
     }
 
     return (
@@ -146,8 +205,9 @@ const Input = ({ setDialogs }) => {
                                     fontSize='large'
                                     style={style.send}
                                 />
-                                <label for="files"><Icon style={style.file} icon="akar-icons:folder" /></label>
+                                <label htmlFor="files"><Icon style={style.file} icon="akar-icons:folder" /></label>
                                 <input id="files" style={{ visibility: "hidden" }} type="file" multiple onChange={handleChange} />
+                                {files.length > 0 && <PreviewSelectedFiles />}
                                 {showEmoji && (
                                     <div style={{ position: "fixed", bottom: "80px", left: "20%"}}>
                                         <Picker
