4defc18d37a3a549ada91801d882cd599942d68a
minhtan <32776120+minhtan4711@users.noreply.github.com>
2021-12-28 00:47:14 +0700

    Feat/update info UI (#29)
    add user ui

--- src/Components/UserInformation/UserArea.js
+++ src/Components/UserInformation/UserArea.js
@@ -2,6 +2,8 @@ import Avatar from '@material-ui/core/Avatar'
 import { makeStyles } from "@material-ui/core"
 // import { useMediaQuery } from 'react-responsive'
 import { Icon } from '@iconify/react'
+import { useState } from 'react'
+import {Modal, Form, Input, Avatar as UserAvatar} from 'antd'
 
 const useStyles = makeStyles((theme) => ({
   large: {
@@ -12,7 +14,15 @@ const useStyles = makeStyles((theme) => ({
 
 const UserArea = ({ user, logout }) => {
   const classes = useStyles()
+  const [updateVisible, setUpdateVisible] = useState(false)
 
+  const handleEditUserInfo = () => {
+    setUpdateVisible(!updateVisible)
+  }
+
+  // TODO: update user info
+  const handleUpdateUserInfo = () => {}
+  const handleUpdateUserAvatar = () => {}
   // const limit = useMediaQuery({ maxWidth: 1300 })
 
   const style = {
@@ -33,6 +43,7 @@ const UserArea = ({ user, logout }) => {
       marginRight: '20px',
       marginLeft: '20px',
       background: '#' + user.color,
+      cursor: 'pointer',
     },
     info: {
       flexGrow: 1,
@@ -58,12 +69,49 @@ const UserArea = ({ user, logout }) => {
       color: '#FFFFFF',
       fontSize: '10px',
 
+    },
+
+    userAvaterEdit: {
+      width: 'fit-content',
+      display: 'block',
+      margin: 'auto',
+      border: '4px solid #' + user.color,
+      borderRadius: '100px',
+      cursor: 'pointer',
     }
   }
 
   return (
     <div style={style.userInformation}>
-      <Avatar style={style.avatar} className={classes.large}>{user.fullname.toUpperCase()[0]}</Avatar>
+      <Avatar onClick={handleEditUserInfo} style={style.avatar} className={classes.large}>{user.fullname.toUpperCase()[0]}</Avatar>
+      
+
+      <Modal visible={updateVisible} closable={false} title="Update User Info" onCancel={handleEditUserInfo} onOk={handleUpdateUserInfo}>
+        {updateVisible &&
+            <div>
+                <label style={style.userAvaterEdit} for="files"><UserAvatar size={200} src="https://joeschmoe.io/api/v1/random"></UserAvatar></label>
+                <input id="files" style={{ visibility: "hidden" }} accept='image/*' type="file" onChange={handleUpdateUserAvatar} />
+            </div>
+        }
+        <Form 
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+        >
+          <Form.Item label="Full Name">
+            <Input defaultValue={user.fullname} />
+          </Form.Item>
+          <Form.Item label="User Name">
+            <Input defaultValue={user.username} />
+          </Form.Item>
+          <Form.Item label="Password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="Confirm Password">
+            <Input.Password />
+          </Form.Item>
+        </Form>
+      </Modal>
+      
       <div style={style.info}>
         <p style={style.fullname}>{user.fullname}</p>
         <p style={style.username}>{user.username}</p>
