5eae1ecf49a4e429200504d0481c0fdb7e26b89a
minhtan <32776120+minhtan4711@users.noreply.github.com>
2022-02-09 23:48:20 +0700

    style/responsive (#63)

--- src/Components/UserInformation/UserArea.js
+++ src/Components/UserInformation/UserArea.js
@@ -7,7 +7,7 @@ import { Modal, Form, Input, Avatar as UserAvatar, Button } from 'antd'
 import { storage } from "../../firebase/index"
 import { toast } from 'react-toastify'
 import { userService } from "../../service/user"
-
+import { useEffect } from 'react'
 const useStyles = makeStyles((theme) => ({
   large: {
     width: theme.spacing(10),
@@ -62,10 +62,16 @@ const UserArea = ({ user, logout }) => {
     }
     setUserAvatar(newUserAvatar)
   }
+  const mediaMatch = window.matchMedia("(min-width: 970px)")
+  const [matches, setMatches] = useState(mediaMatch.matches);
   // const limit = useMediaQuery({ maxWidth: 1300 })
-
+  useEffect(() => {
+    const handler = e => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  });
   const style = {
-    userInformation: {
+    userInformation: isRowBased => ({
       display: 'flex',
       alignItems: 'center',
       alignContent: 'flex-start',
@@ -73,36 +79,34 @@ const UserArea = ({ user, logout }) => {
       height: '100px',
       width: '100%',
       backgroundColor: '#E3F6FC',
-      // borderRadius: '14px',
       marginBottom: '10px',
-      // marginLeft: '10px',
-    },
-    avatar: {
-      fontSize: 'xx-large',
-      marginRight: '20px',
-      marginLeft: '20px',
+    }),
+    avatar: isRowBased => ({
+      // fontSize: isRowBased ? '24px' : '8px',
+      marginRight: isRowBased ? '20px' : '10px',
+      marginLeft: isRowBased ? '20px' : '10px',
       background: '#' + user?.color,
       cursor: 'pointer',
-    },
+    }),
     info: {
       flexGrow: 1,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'start',
     },
-    fullname: {
+    fullname: isRowBased => ({
       fontFamily: 'Poppins',
       fontStyle: 'normal',
       fontWeight: '600',
-      fontSize: '20px',
-      /* identical to box height */
+      fontSize: isRowBased ? '18px' : '12px',
       color: '#52585D',
-      margin: '0px',
-    },
+      margin: isRowBased ? '0px' : '5px',
+      width: '100%'
+    }),
     username: {
       fontStyle: 'normal',
       fontFamily: 'Poppins',
-      padding: '5px 10px',
+      padding: '5px 8px',
       backgroundColor: '#6588DE',
       borderRadius: '14px',
       color: '#FFFFFF',
@@ -121,8 +125,8 @@ const UserArea = ({ user, logout }) => {
   }
 
   return (
-    <div style={style.userInformation}>
-      <Avatar onClick={handleEditUserInfo} style={style.avatar} className={classes.large} src={user?.avatar}></Avatar>
+    <div style={style.userInformation(matches)}>
+      <Avatar onClick={handleEditUserInfo} style={style.avatar(matches)} className={classes.large} src={user?.avatar}></Avatar>
 
       <Modal visible={updateVisible} closable={false} title="Update User Info" onCancel={handleEditUserInfo} footer={null}>
         {updateVisible &&
@@ -188,10 +192,10 @@ const UserArea = ({ user, logout }) => {
       </Modal>
 
       <div style={style.info}>
-        <p style={style.fullname}>{user?.fullname}</p>
+        <p style={style.fullname(matches)}>{user?.fullname}</p>
         <p style={style.username}>{user?.username}</p>
       </div>
-      <Icon icon="ion:log-out-outline" style={{ fontSize: '30px', marginRight: '20px', marginLeft: '20px', cursor: 'pointer' }} onClick={logout} />
+      <Icon icon="ion:log-out-outline" style={{ fontSize: '30px', cursor: 'pointer' }} onClick={logout} />
     </div>
   )
 }
